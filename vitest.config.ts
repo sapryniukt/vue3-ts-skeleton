43a92e3aa86b61c68dd42789a084218c20d077d1
Taras Sapryniuk <85492017+sapryniukt@users.noreply.github.com>
2024-05-26 20:01:27 +0200

    Configure Sonar (#9)
    * Configure Sonar
    
    * Combine actions
    
    * Minor update

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'lcov'],
+			reportsDirectory: './coverage',
+		},
 	},
 })
